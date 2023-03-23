3d785063701cec3c3e2f93ebd805cbe6bcf6eccd
DylanTHK <dylantanhk@gmail.com>
2023-03-23 12:00:03 +0800

    added Reactive Forms for city input

--- day34-workshop/src/app/app.component.ts
+++ day34-workshop/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'day34-workshop';
+export class AppComponent implements OnInit {
+  title = 'Day34 Workshop - Weather App';
+
+  form!: FormGroup;
+
+  constructor(private fb:FormBuilder) { };
+
+  ngOnInit(): void {
+      this.form = this.fb.group({
+        city: this.fb.control('singapore', [Validators.required])
+      });
+  }
+
+  getWeather() {
+    const city = this.form.get("city")?.value;
+    console.info(">>> City Detected: ", city);
+  }
 }
